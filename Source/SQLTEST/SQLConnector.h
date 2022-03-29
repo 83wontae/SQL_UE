93cb3db3b7e5d8740507d531a8fb11abf479e795
wontae <83wontae@gmail.com>
2022-03-29 13:25:28 +0900

    aaa

---
+++ Source/SQLTEST/SQLConnector.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SQLConnector.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQLTEST_API USQLConnector : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	
+	void TestSQL();
+
+};
